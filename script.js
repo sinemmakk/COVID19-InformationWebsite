159635ca3a94bff7ffccfa67f15b68b219ee12e3
Sinem Ak <32371584+sinemmakk@users.noreply.github.com>
2020-05-20 14:47:30 +0300

    JS File

---
+++ script.js
@@ -0,0 +1,25 @@
+// For Video Page
+$('#video1').on('hidden.bs.modal', function (e) {
+  $('#video1 iframe').attr("src", $("#video1 iframe").attr("src"));
+});
+
+$('#video2').on('hidden.bs.modal', function (e) {
+  $('#video2 iframe').attr("src", $("#video2 iframe").attr("src"));
+});
+
+$('#video3').on('hidden.bs.modal', function (e) {
+  $('#video3 iframe').attr("src", $("#video3 iframe").attr("src"));
+});
+
+$('#video4').on('hidden.bs.modal', function (e) {
+  $('#video4 iframe').attr("src", $("#video4 iframe").attr("src"));
+});
+
+$('#video5').on('hidden.bs.modal', function (e) {
+  $('#video5 iframe').attr("src", $("#video5 iframe").attr("src"));
+});
+
+$('#video6').on('hidden.bs.modal', function (e) {
+  $('#video6 iframe').attr("src", $("#video6 iframe").attr("src"));
+});
+
